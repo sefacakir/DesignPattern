b63e2e178689d34aa97ef7351e60c38e390a1348
sefacakir <cakirsefa72@gmail.com>
2021-11-30 21:58:02 +0300

    Singleton design pattern added

---
+++ Singleton/MailBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class MailBox
+    {
+        private int _size;
+        private string _backgroundColor;
+        private static MailBox mailBox = new MailBox();
+        private MailBox()
+        {
+            _backgroundColor= "black";
+            _size = 10;
+        }
+
+        public static MailBox GetInstance()
+        {
+            return mailBox;
+        }
+        public void PrintInformation()
+        {
+            Console.WriteLine("Size: "+_size);
+            Console.WriteLine("Background Color: "+_backgroundColor);
+            Console.WriteLine("\n");
+        }
+        public void SetBox(int size, string backgroundColor)
+        {
+            _size = size;
+            _backgroundColor = backgroundColor;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailBox mailBox = MailBox.GetInstance();
+            mailBox.PrintInformation();
+
+            MailBox mailBox1 = MailBox.GetInstance();
+            mailBox.PrintInformation();
+
+            mailBox1.SetBox(20,"white");
+            mailBox.PrintInformation();
+            mailBox1.PrintInformation();
+        }
+    }
+}
