6f0e18e230c250ee11c949c17f2893adf8c07bbc
sefacakir <cakirsefa72@gmail.com>
2021-12-29 21:00:32 +0300

    Complete Chain of Responsibility

--- Chain_of_Responsibility/Program.cs
+++ Chain_of_Responsibility/Program.cs
@@ -6,6 +6,82 @@ namespace Chain_of_Responsibility
     {
         static void Main(string[] args)
         {
+            File file = new File("MyFile", "text");
+            
+            TextHandler textHandler = new TextHandler();
+            WordHandler wordHandler = new WordHandler();
+            OtherHandler otherHandler = new OtherHandler();
+
+            textHandler.SetNextHandler(wordHandler);
+            textHandler.SetNextHandler(otherHandler);
+
+            textHandler.HandleRequest(file);
+
         }
     }
+
+    class File
+    {
+        public string _name;
+        public string _extension;
+
+        public File(string name, string extension)
+        {
+            _name = name;
+            _extension = extension;
+        }
+    }
+    
+    abstract class BaseHandler
+    {
+        protected BaseHandler _nextHandler;
+        public void SetNextHandler(BaseHandler nextHandler)
+        {
+            _nextHandler = nextHandler;
+        }
+
+        public abstract void HandleRequest(File file);
+    }
+
+    class TextHandler : BaseHandler
+    {
+        public override void HandleRequest(File file)
+        {
+            if(file._extension == "text")
+            {
+                Console.WriteLine("text to PDF");
+            }
+            else
+            {
+                _nextHandler.HandleRequest(file);
+            }
+        }
+    }
+
+    class WordHandler : BaseHandler
+    {
+        public override void HandleRequest(File file)
+        {
+            if(file._extension == "word")
+            {
+                Console.WriteLine("Word to pdf");
+            }
+            else
+            {
+                _nextHandler.HandleRequest(file);
+            }
+        }
+    }
+
+    class OtherHandler : BaseHandler
+    {
+        public override void HandleRequest(File file)
+        {
+            if(file._extension == "other")
+            {
+                Console.WriteLine("Other to pdf");
+            }
+        }
+    }
+
 }
